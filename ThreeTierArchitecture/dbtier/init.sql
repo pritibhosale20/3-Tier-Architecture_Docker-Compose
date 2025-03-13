cd34677a2e5ac944a2d93a1bed65e2fc00025bcf
Priti Bhosale <pritibhosale123123@gmail.com>
2025-03-13 21:36:11 +0530

    Update init.sql

--- ThreeTierArchitecture/dbtier/init.sql
+++ ThreeTierArchitecture/dbtier/init.sql
@@ -1,4 +1,4 @@
-create database insta;
-use insta;
-create table users(id int , name varchar(20));
-insert into users values(1,'ravi'),(2,'aliya');
+create database facebook;
+use facebook;
+create table users(id int , name varchar(30));
+insert into users values(1,'priti'),(2,'advik');
